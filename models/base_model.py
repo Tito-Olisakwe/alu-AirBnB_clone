75795233b023bf85f6cf089612ecbb1ba454bd78
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-05-16 19:27:27 +0200

    BaseModel class test

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = None
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -18,5 +18,6 @@ class BaseModel:
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
+        if self.updated_at is not None:
+            obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
---
+++ tests/test_base_model.py
@@ -0,0 +1,35 @@
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+class TestBaseModel(unittest.TestCase):
+    def test_attributes(self):
+        """Test the attributes of the BaseModel class"""
+        bm = BaseModel()
+        self.assertIsInstance(bm.id, str)
+        self.assertIsInstance(bm.created_at, datetime)
+        self.assertIsNone(bm.updated_at)
+
+    def test_str(self):
+        """Test the __str__ method of the BaseModel class"""
+        bm = BaseModel()
+        self.assertIsInstance(bm.__str__(), str)
+
+    def test_save(self):
+        """Test the save method of the BaseModel class"""
+        bm = BaseModel()
+        initial_updated_at = bm.updated_at
+        bm.save()
+        self.assertNotEqual(initial_updated_at, bm.updated_at)
+
+    def test_to_dict(self):
+        """Test the to_dict method of the BaseModel class"""
+        bm = BaseModel()
+        bm_dict = bm.to_dict()
+        self.assertIsInstance(bm_dict, dict)
+        self.assertEqual(bm_dict["__class__"], "BaseModel")
+        self.assertEqual(bm_dict["id"], bm.id)
+        self.assertEqual(bm_dict["created_at"], bm.created_at.isoformat())
+        if bm.updated_at is not None:
+            self.assertEqual(bm_dict['updated_at'], bm.updated_at.isoformat())
+            
