48d806097098cfca3180a7eca9df3ad96c86b2ea
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2023-05-17 06:05:28 +0000

    task 4 correction

--- models/base_model.py
+++ models/base_model.py
@@ -6,14 +6,15 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
+        self.updated_at = None
 
         if kwargs:
-            kwargs.pop('__class__', None)
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            super().__init__(**kwargs)
-        else:
-            super().__init__()
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -22,12 +23,10 @@ class BaseModel:
         self.updated_at = datetime.now()
     
     def to_dict(self):
-        """Return a dictionary representation of the instance"""
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
         if self.updated_at is not None:
-            obj_dict['updated_at'] = self.updated_at.isoformat()
-        else:
-            obj_dict['updated_at'] = None
+            obj_dict["updated_at"] = self.updated_at.isoformat()
         return obj_dict
