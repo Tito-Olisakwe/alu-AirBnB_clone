cfd6be194e26585b9b3a251620456825d25f2719
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2023-05-17 06:10:51 +0000

    task 4 correction

--- models/base_model.py
+++ models/base_model.py
@@ -18,13 +18,16 @@ class BaseModel:
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+  
     def save(self):
         self.updated_at = datetime.now()
-    
+   
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat() if self.updated_at else None
+        if self.updated_at is not None:
+            obj_dict["updated_at"] = self.updated_at.isoformat()
+        else:
+            obj_dict.pop("updated_at", None)
         return obj_dict
