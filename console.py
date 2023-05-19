6c4ecf1c75614d20831072eee6cc866826cb3a5c
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2023-05-19 13:49:07 +0000

    update console correction

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 "Entry point of the command interpreter"
 import cmd
+import shlex
+import models
 from models.base_model import BaseModel
 from models import storage
 
@@ -99,40 +101,28 @@ class HBNBCommand(cmd.Cmd):
         print([str(instance) for instance in instances])
 
     def do_update(self, arg):
-        """Updates an instance based on the class name and id"""
-        args = arg.split()
-        if not args:
+        """
+        Updates an instance based on the class name and id
+        by adding or updating attribute
+        """
+        args = shlex.split(arg)
+        if not arg:
             print("** class name missing **")
-            return
-
-        class_name = args[0]
-        if class_name not in storage.all():
+        elif args[0] not in models.classes:
             print("** class doesn't exist **")
-            return
-
-        if len(args) < 2 or not args[1].strip():
+        elif len(args) < 2:
             print("** instance id missing **")
-            return
-
-        instance_id = args[1]
-        key = class_name + '.' + instance_id
-        if key not in storage.all():
-            print("** no instance found **")
-            return
-
-        if len(args) < 3 or not args[2].strip():
+        elif len(args) < 3:
             print("** attribute name missing **")
-            return
-
-        if len(args) < 4 or not args[3].strip():
+        elif len(args) < 4:
             print("** value missing **")
-            return
-
-        attribute_name = args[2]
-        attribute_value = args[3].strip('"')
-        obj = storage.all()[key]
-        setattr(obj, attribute_name, attribute_value)
-        obj.save()
+        else:
+            key = args[0] + "." + args[1]
+            if key not in models.storage.all():
+                print("** no instance found **")
+            else:
+                setattr(models.storage.all()[key], args[2], args[3])
+                models.storage.save()
 
 
 if __name__ == '__main__':
