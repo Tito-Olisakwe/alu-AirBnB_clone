2bae9da66782cafa5c4b639ad0bad2d64f70de84
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2023-05-19 11:19:09 +0000

    console

---
+++ console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '  # Set the custom prompt
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program on EOF (Ctrl+D)"""
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
