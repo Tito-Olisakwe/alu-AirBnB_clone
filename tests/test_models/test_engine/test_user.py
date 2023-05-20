336f0563bb3d9c6f2cb79502bc27b9f59173339c
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-05-20 23:17:06 +0200

    test_user

--- tests/test_models/test_engine/test_user.py
+++ tests/test_models/test_engine/test_user.py
@@ -1,36 +1,59 @@
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
-    def setUp(self):
-        # Create an instance of User for testing
-        self.user = User()
+    """Test User class."""
 
-    def tearDown(self):
-        # Clean up after each test method
-        del self.user
+    def test_instance(self):
+        """test instance."""
+        user = User()
+        self.assertIsInstance(user, User)
 
-    def test_user_attributes(self):
-        # Test the initial attributes of User
-        self.assertEqual(self.user.email, "")
-        self.assertEqual(self.user.password, "")
-        self.assertEqual(self.user.first_name, "")
-        self.assertEqual(self.user.last_name, "")
+    def test_is_class(self):
+        """test is a class type."""
+        user = User()
+        self.assertEqual(str(type(user)),
+                         "<class 'models.user.User'>")
 
-    def test_user_to_dict(self):
-        # Test the to_dict() method of User
-        user_dict = self.user.to_dict()
-        self.assertEqual(user_dict["__class__"], "User")
-        self.assertEqual(user_dict["email"], "")
-        self.assertEqual(user_dict["password"], "")
-        self.assertEqual(user_dict["first_name"], "")
-        self.assertEqual(user_dict["last_name"], "")
+    def test_is_subclass(self):
+        """test is_subclass."""
+        user = User()
+        self.assertTrue(issubclass(type(user), BaseModel))
 
-    def test_user_str_representation(self):
-        # Test the __str__() method of User
-        expected_str = "[User] ({}) {}".format(self.user.id, self.user.__dict__)
-        self.assertEqual(str(self.user), expected_str)
+    def test_id(self):
+        """test email."""
+        user = User()
+        self.assertIsNotNone(user.id)
+
+    def test_email(self):
+        """test email."""
+        user = User()
+        self.assertEqual(user.email, "")
+        user.email = "airbnb@mail.com"
+        self.assertEqual(user.email, "airbnb@mail.com")
+
+    def test_password(self):
+        """test password."""
+        user = User()
+        self.assertEqual(user.password, "")
+        user.password = "root"
+        self.assertEqual(user.password, "root")
+
+    def test_first_name(self):
+        """test first name."""
+        user = User()
+        self.assertEqual(user.first_name, "")
+        user.first_name = "Betty"
+        self.assertEqual(user.first_name, "Betty")
+
+    def test_last_name(self):
+        """test last name."""
+        user = User()
+        self.assertEqual(user.last_name, "")
+        user.first_name = "Bar"
+        self.assertEqual(user.first_name, "Bar")
 
 
 if __name__ == "__main__":
