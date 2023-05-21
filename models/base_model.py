71b75f590d6792e66ce7556b8c4ca930579d1a4d
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2023-05-21 22:57:55 +0000

    pep8

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import uuid
 import models
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
@@ -14,22 +15,23 @@ class BaseModel:
                 if key == '__class__':
                     continue
                 setattr(self, key, value)
+          
                 if key in ['created_at', 'updated_at']:
-                     # Convert the string value to a datetime object
+                    # Convert the string value to a datetime object
                     setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-            
+   
         # If it's a new instance, add a call to the method new(self) on storage
         if not kwargs:
             models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-  
+
     def save(self):
         self.updated_at = datetime.now()
         # Call the save(self) method for the storage instance
         models.storage.save()
-   
+
     def to_dict(self):
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
@@ -37,5 +39,6 @@ class BaseModel:
         if self.updated_at is not None:
             obj_dict["updated_at"] = self.updated_at.isoformat()
         else:
-            obj_dict.pop("updated_at", None) # Remove 'updated_at' key if it's None
+            # Remove 'updated_at' key if it's None
+            obj_dict.pop("updated_at", None)
         return obj_dict
